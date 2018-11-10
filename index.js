01f474377ba6bf7c30d5f1573d2f4dd26d8169ad
Connor Boyle <cjblink1@gmail.com>
2018-11-10 16:27:04 -0500

    Card reader gets UID

--- index.js
+++ index.js
@@ -11,7 +11,37 @@ pcsc.on('reader', function(reader) {
     });
 
     reader.on('status', function(status) {
-        console.log(status)
+        // console.log('Status(', this.name, '):', status);
+        /* check what has changed */
+        var changes = this.state ^ status.state;
+        if (changes) {
+            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+                console.log("card removed");/* card removed */
+                reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Disconnected');
+                    }
+                });
+            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+                console.log("card inserted");/* card inserted */
+                reader.connect({ share_mode : this.SCARD_SHARE_SHARED }, function(err, protocol) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log('Protocol(', reader.name, '):', protocol);
+                        reader.transmit(Buffer.from([0xFF, 0xB0, 0x00, 0x00, 0x00]), 40, protocol, function(err, data) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log('Data received', data.toString('hex'));
+                            }
+                        });
+                    }
+                });
+            }
+        }
     });
 
 
